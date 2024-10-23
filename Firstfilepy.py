eac7d6efdd0552efb091de232b8e7ab45fd77320
Vaidehi Nayakpura <149557062+Vaidehi1005@users.noreply.github.com>
2024-10-23 23:16:05 +0530

    Create Firstfilepy.py

---
+++ Firstfilepy.py
@@ -0,0 +1,116 @@
+!python --version
+Python 3.10.12
+
+print("Hello Friend")
+Hello Friend
+
+from google.colab import drive
+drive.mount('/gdrive')
+Mounted at /gdrive
+
+
+print("Hello\\")
+Hello\
+
+print("wer\rVaidu")
+Vaidu
+
+print("Hello\tVaidehi")
+Hello	Vaidehi
+
+print("Hello\nVaidehi")
+Hello
+Vaidehi
+
+print("he\byu")
+hyu
+
+c=input()
+print(type(c))
+23
+<class 'str'>
+
+name=input("What is your name:")
+print(name*10)
+What is your name:parthu
+parthuparthuparthuparthuparthuparthuparthuparthuparthuparthu
+
+num1=int(input())
+num2=int(input())
+sum=num1+num2
+print(sum)
+23
+45
+68
+
+#calculate the area of triangle using python
+x=float(input("Enter first side:"))
+y=float(input("Enter second side:"))
+z=float(input("Enter third side:"))
+print(x*y*z)
+print("Area of triangle is : ",x*y*z)
+Enter first side:4
+Enter second side:8
+Enter third side:10
+320.0
+Area of triangle is :  320.0
+
+x=8
+X=9
+print(x,X)
+8 9
+
+
+a=b=c="vaidehi"
+print(a,b,c)
+vaidehi vaidehi vaidehi
+
+a="python"
+print("I like " + a)
+I like python
+
+#this is a comment
+print("Hello friend. My name Vaidehi Nayakpura.")
+Hello friend. My name Vaidehi Nayakpura.
+
+"""This is a multiple line comment"""
+print("Hello friend. My name Vaidehi Nayakpura.")
+Hello friend. My name Vaidehi Nayakpura.
+
+#single quote
+print("It\'s alright.")
+It's alright.
+
+#backslash mate
+print("C:\\Users\\Vaidehi\\Desktop\\Vaidehi")
+C:\Users\Vaidehi\Desktop\Vaidehi
+
+#new line ma jase
+print("hello\nworld")
+hello
+world
+
+#remove karse e aagad na word ne
+print("hello\rworld")
+world
+
+#tab
+print("hello\tworld")
+hello	world
+
+#backspace
+print("hello\bworld")
+hellworld
+
+#form feed
+print("hello\fWorld")
+#form feed
+
+#octal values
+print("\101\102\103\104")
+ABCD
+
+#hexadecimal values
+print("\x48\x65\x6c\x6c\x6f")
+Hello
+
